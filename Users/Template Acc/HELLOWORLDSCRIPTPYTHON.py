e63b365332806445500f4e5b850dee86b6e697f2
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-07-01 10:37:05 -0700

    first 100 files
    The first 100 files at root to upload here. Files are in various formats.

---
+++ Users/Template Acc/HELLOWORLDSCRIPTPYTHON.py
@@ -0,0 +1,33 @@
+# start of script
+print ("Hello world!") # English
+print ("Hola Mundo!") # Spanish
+print ("Bonjour le monde!") # French
+print ("Hallo Welt") # German 
+print ("Привет, мир") # Russian 
+print ("Përshendetje Botë") # Albanian 
+print ("Hola món") # Catalan 
+print ("你好，世界") # Simplified Chinese 
+print ("你好，世界") # Traditional Chinese 
+print ("Hallo Wereld") # Dutch 
+print ("Halo Dunia") # Indonesian 
+print ("שלום עולם") # Hebrew 
+print ("Ciao mondo") # Italian 
+print ("Chào thế giới") # Vietnamese
+print ("Helo Byd") # Welsh 
+print ("Hej världen") # Swedish 
+print ("こんにちは世界") # Japanese 
+print ("ahoj svet") # Slovak
+print ("Selam Dünya") # Turkish
+print ("Olá Mundo") # Portugese
+noMore = input("Please press [ENTER] key to quit") # end of program
+'''
+Please report accuracy
+Non-english translations done with Google Translate
+'''
+'''
+Hello world in 20 languages
+feel free to correct and add in more languages if needed
+Open-source project by @seanwallawalla
+copyleft 2015-2019
+'''
+# end of script
